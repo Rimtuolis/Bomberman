bd3ddbd48c4cb69babcd48e9876814aff45c7df6
ErnestasLabanauskas1 <elabanauskas7@gmail.com>
2023-10-12 01:04:21 +0300

    Added Bomb/Box/BrickWall classes

---
+++ Shared/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberGopnik.Shared
+{
+    public class Box
+    {
+        public int StartX { get; set; }
+        public int Length { get; set; }
+        public int StartY { get; set; }
+
+    }
+}
