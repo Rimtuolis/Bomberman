f6ed266d627296dbce82d7406ce5b3e0ce2e14c7
ErnestasLabanauskas1 <elabanauskas7@gmail.com>
2023-11-09 09:03:59 +0200

    Abstract Factory

---
+++ Shared/BreakableGlass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberGopnik.Shared
+{
+    public class BreakableGlass : Glass
+    {
+        public int StartX { get; set; }
+        public int Length { get; set; }
+        public int StartY { get; set; }
+        public int PowerUp { get; set; }
+
+        public void Build()
+        {
+            Console.WriteLine("Glass was built");
+        }
+
+        public int GetLength()
+        {
+            return Length;
+        }
+
+        public int GetStartX()
+        {
+            return StartX;
+        }
+
+        public int GetStartY()
+        {
+            return StartY;
+        }
+    }
+}
---
+++ Shared/UnBreakableFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberGopnik.Shared
+{
+    public class UnBreakableFactory : AbstractFactory
+    {
+        public  Glass CreateGlass(int startX, int startY, int length)
+        {
+            return new UnBreakableGlass { StartX = startX, Length = length, StartY = startY };
+        }
+
+        public  Door CreateDoor(int startX, int startY, int length)
+        {
+            return new UnBreakableDoor { StartX = startX, Length = length, StartY = startY };
+        }
+    }
+}
