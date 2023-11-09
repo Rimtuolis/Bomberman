fa15c09f29483118a6056d72eb1de4ff7718f9c5
Gvidas Bertašius <gvidas@esoftware.solutions>
2023-11-09 06:21:08 +0200

    observeris

--- Shared/Player.cs
+++ Shared/Player.cs
@@ -1,4 +1,6 @@
-﻿namespace BomberGopnik.Shared
+﻿using System.Drawing;
+
+namespace BomberGopnik.Shared
 {
 	public class Player
     {
@@ -7,6 +9,7 @@
 		public double Top { get; set; }
 		public double Left { get; set; }
 		public int Points { get; set; }
+		public bool IsPaused { get; set; }
 
 		public Player(string connectionId, string color, double top, double left, int points)
 		{
@@ -16,6 +19,19 @@
 			Left = left;
 			Points = points;
 		}
-	}
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Player p = (Player)obj;
+                return p.ConnectionId == this.ConnectionId;
+            }
+        }
+    }
 
 }
