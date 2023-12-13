9d3b719f147e1de80fdc85a5892b095ff3155eda
Augustas Jonaitis <augustas.jonaitis1@gmail.com>
2023-12-13 22:15:39 +0200

    added missing files

---
+++ Shared/ChainsOfResponsibility/BoxHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberGopnik.Shared.ChainsOfResponsibility
+{
+    public class BoxHandler : IStructureHandler
+    {
+        private IStructureHandler nextHandler;
+
+        List<IStructure> structures = new List<IStructure>();
+        public void SetNext(IStructureHandler handler)
+        {
+            nextHandler = handler;
+        }
+
+        public List<IStructure> HandleRequest(params object[] parameters)
+        {
+            int startX = (int)parameters[0];
+            int startY = (int)parameters[1];
+            int length = (int)parameters[2];
+            int powerUp = (int)parameters[3];
+            List<IStructure> structures = new List<IStructure>();
+            // Check if this handler can handle the request for creating boxes
+            if (length != 0)
+            {
+                var boxFactory = new BoxFactory();
+                return new List<IStructure> { boxFactory.CreateStructure(startX, startY, length, powerUp)};
+            }
+            else if (nextHandler != null)
+            {
+                // Pass the request to the next handler in the chain
+                return nextHandler.HandleRequest(parameters);
+            }
+            else
+            {
+                // No handler found to handle this request
+                return new List<IStructure>(); // Or handle the case appropriately
+            }
+        }
+    }
+}
---
+++ Shared/ChainsOfResponsibility/BrickHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberGopnik.Shared.ChainsOfResponsibility
+{
+    public class BrickHandler : IStructureHandler
+    {
+        private IStructureHandler nextHandler;
+
+        public void SetNext(IStructureHandler handler)
+        {
+            nextHandler = handler;
+
+        }
+
+        public List<IStructure> HandleRequest(params object[] parameters)
+        {
+            string structureType = (string)parameters[0];
+            int x = (int)parameters[1];
+            int y = (int)parameters[2];
+            int size = (int)parameters[3];
+
+            // Check if this handler can handle the request for creating bricks
+            if (structureType.Equals("brickwall"))
+            {
+                var factory = new StructureFactory();
+                return new List<IStructure> { factory.CreateStructure("brickwall", x, y, size) };
+            }
+            else if (nextHandler != null)
+            {
+                // Pass the request to the next handler in the chain
+                return nextHandler.HandleRequest(parameters);
+            }
+            else
+            {
+                // No handler found to handle this request
+                return new List<IStructure>(); // Or handle the case appropriately
+            }
+        }
+    }
+
+}
